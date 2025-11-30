1fb4df8be1f1e13b5c7b92065e7afb696419a8fc
dkirich5 <dkirich23@gmail.com>
2025-11-30 13:38:42 -0800

    pa6-2

---
+++ FactoryDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.Common;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string provider = "SqlServer";
+        string connectionString = "Data Source=(localdb)\\mssqllocaldb;Integrated Security=true;Initial Catalog=AutoLot2023";
+
+        DbProviderFactory factory = SqlClientFactory.Instance;
+
+        using DbConnection connection = factory.CreateConnection();
+        connection.ConnectionString = connectionString;
+        connection.Open();
+
+        Console.WriteLine($"provider is: {provider}");
+        Console.WriteLine($"connection string: {connectionString}");
+        Console.WriteLine($"Your connection object is a: {connection.GetType().Name}");
+
+        using DbCommand command = connection.CreateCommand();
+        command.CommandText = "SELECT Id, MakeId, Color, PetName FROM Inventory";
+
+        Console.WriteLine($"Your command object is a: {command.GetType().Name}");
+
+        using DbDataReader reader = command.ExecuteReader();
+        Console.WriteLine($"Your data reader object is a: {reader.GetType().Name}");
+
+
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(reader.GetOrdinal("Id"));
+            int makeId = reader.GetInt32(reader.GetOrdinal("MakeId"));
+            string color = reader.GetString(reader.GetOrdinal("Color"));
+            string petName = reader.GetString(reader.GetOrdinal("PetName"));
+
+            Console.WriteLine($"-> Car #{id}: MakeId={makeId}, Color={color}, PetName={petName}");
+        }
+    }
+}
---
+++ Module6/ConfigDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConfigDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var (provider, conn) = GetProviderFromConfiguration();
+
+            Console.WriteLine($"Provider: {provider}");
+            Console.WriteLine($"Connection String: {conn}");
+        }
+
+        static (string Provider, string ConnectionString) GetProviderFromConfiguration()
+        {
+            // Building the configuration object
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            // Reading values from the config file
+            string provider = config["ProviderName"];
+            string connStr = config["SqlServer:ConnectionString"];
+
+            return (provider, connStr);
+        }
+    }
+}
